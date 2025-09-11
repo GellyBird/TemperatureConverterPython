f73208a9f7e6103ea029278460d23a8db19f65fc
GellyBird <147560865+GellyBird@users.noreply.github.com>
2025-09-11 10:14:27 -0400

    Update TemperatureConverter.py

--- TemperatureConverter.py
+++ TemperatureConverter.py
@@ -1,6 +1,6 @@
 
 orig_temp = int(input("Please enter a temperature in Fahrenheit: "))
 
-new_temp = int((orig_temp - 32)*(5/9))
+new_temp = round((orig_temp - 32)*(5/9))
 
 print(str(orig_temp)+"F","in Celsius is:",str(new_temp)+"C")
